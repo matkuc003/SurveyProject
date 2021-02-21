6951cda6989241a8344bdc30ef6bd23b2f48c705
matkuc003 <mnorris.k97@gmail.com>
2021-02-21 22:05:08 +0100

    deleted the folder

--- codemr/survey/html/main_report/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"survey", "Number of Highly Problematic Classes":0, "Number of Entities":38, "Number of Problematic Classes":0, "Class Lines of Code":774, "Number of External Packages":47, "Number of Packages":9, "Number of External Entities":137}
